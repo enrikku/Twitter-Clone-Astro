354eaa4a51f9dcd4ace156f8b5a19cad4e34916e
Enric Cabeza Perez <91900904+enrikku@users.noreply.github.com>
2024-06-11 16:16:15 +0200

    Varios cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,13 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors:{
+			'transparent': 'transparent',
+			'twitter-blue': '#1c95e6',
+			'black': '#000000',
+			'white': '#ffffff',
+			'gray': '#9ca3af',
+		}
 	},
 	plugins: [],
 }
